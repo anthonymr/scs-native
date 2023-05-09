f4838b08588d87b672ae10787efeb0d56f142248
anthonymr <anthonymartin20@gmail.com>
2023-05-09 17:42:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# scs-native
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nativescript-stackblitz-templates-yrqtcb)
---
+++ app/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <Page>
+    <ActionBar>
+      <Label text="Home" class="font-bold text-lg"/>
+    </ActionBar>
+
+    <GridLayout>
+      <Label class="text-xl align-middle text-center text-gray-500" :text="message" @tap="logMessage" />
+    </GridLayout>
+  </Page>
+</template>
+
+<script lang="ts">
+  import Vue from "nativescript-vue";
+
+  export default Vue.extend({
+    computed: {
+      message() {
+        return "Blank {N}-Vue app";
+      }
+    },
+
+    methods: {
+      logMessage() {
+        console.log('You have tapped the message!')
+      }
+    }
+  });
+</script>
+
+<style>
+  /* .info {
+    font-size: 20;
+  } */
+</style>
